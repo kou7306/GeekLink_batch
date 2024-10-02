68f6138a5f771f1fb21c7f6699ee41354392bf33
kou7306 <kouta.1206.y@icloud.com>
2024-10-02 16:14:58 +0900

    cd

--- src/controllers/rankingController.ts
+++ src/controllers/rankingController.ts
@@ -1,9 +1,21 @@
 import { Request, Response } from "express";
-import { updateContributionRankingService } from "../services/rankingService.js";
+import {
+  updateContributionRankingService,
+  updateDailyContributionRankingService,
+} from "../services/rankingService.js";
 
 export const updateRanking = async (req: Request, res: Response) => {
   try {
     await updateContributionRankingService();
+    res.status(200).json({ message: "Likes created successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error", details: error });
+  }
+};
+
+export const updateDailyRanking = async (req: Request, res: Response) => {
+  try {
+    await updateDailyContributionRankingService();
     res.status(200).json({ message: "Likes created successfully" });
   } catch (error) {
     res.status(500).json({ error: "Internal Server Error", details: error });
